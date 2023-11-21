abd866cfb9b920a522da4073295e654ae20648ad
8ohs <107598161+8ohs@users.noreply.github.com>
2023-11-22 00:55:25 +0900

    Delete Card.py

--- Card.py
+++
@@ -1,9 +0,0 @@
-class Card:
-    def get(self, key):
-        return self.info[key]
-
-    def __init__(self, color, special, number):
-        self.info = {'color': color, 'special': special, 'number': number}
-
-    def setColor(self,color):
-        self.info['color'] = color
